5c304c355765a174a8ae66305212568368e9207d
Simon Halimonov <hello@simonhalimonov.de>
2024-07-17 20:53:47 +0700

    fix: test new private key logic

--- src/utils/firebase/adminSDK.ts
+++ src/utils/firebase/adminSDK.ts
@@ -1,13 +1,23 @@
 import { cert, getApps, initializeApp, App } from "firebase-admin/app";
 
+const getPrivateKey = (): string => {
+    // Private ket must not be accessible client-side
+    // Using JSON to handle newline problems when storing the key as a secret in Vercel. See:
+    // https://github.com/vercel/vercel/issues/749#issuecomment-707515089
+    // If in next.js development mode, don't use JSON.parse
+    const privateKey = 
+        process.env.NODE_ENV === 'development'
+            ? process.env.FIREBASE_PRIVATE_KEY
+            : JSON.parse(process.env.FIREBASE_PRIVATE_KEY as string)
+    if (!privateKey) throw new Error('No private key found in environment variables')
+    return privateKey;
+}
+
 const firebaseAdminInitConfig = {
     credential: {
         projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID as string,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL as string,
-        // Private ket must not be accessible client-side
-        // Using JSON to handle newline problems when storing the key as a secret in Vercel. See:
-        // https://github.com/vercel/vercel/issues/749#issuecomment-707515089
-        privateKey: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY : undefined,
+        privateKey: getPrivateKey(),
     },
     databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL as string,
     storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET as string,
